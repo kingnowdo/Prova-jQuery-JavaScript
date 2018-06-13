1b75c098cd28558a3b88c3a3886d9ef9d3efff72
kingnowdo <reinaldomagas@gmail.com>
2018-06-13 13:29:54 -0300

    inseridos os cabeçalhos de todas as funções

--- Trabalho DAW/games.js
+++ Trabalho DAW/games.js
@@ -1,3 +1,20 @@
-$('#e01').click(function() {
-    $(this).toggleClass('rotate');
-});
+function regras(){ //Exibir as regras
+
+}
+
+function compartilhar(){
+
+}
+
+function inserir_nome(){ //Função que pega o nome do campo de texto e coloca no rodapé
+
+}
+
+function animação(item){
+
+}
+
+function clique(item){
+    animação(item); //Uma animação para ser executada quando for dado um clique
+
+}
